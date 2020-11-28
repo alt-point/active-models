acca19ab24a7b6e7e6cf8b03653e3b334206e959
Alex D. Bubenchikov SurRealistik <surrealistik74@gmail.com>
2020-11-29 00:53:13 +0500

    refactor(CallableModel): Added check for the existence of the function `__call` on class heir

--- src/CallableModel.js
+++ src/CallableModel.js
@@ -1,6 +1,17 @@
-export default class Callable extends Function {
+/**
+ * Callable class
+ */
+export default class CallableModel extends Function {
+  __call () {
+    throw new TypeError('Method "__call" must be implemented')
+  }
+
   constructor () {
     super()
+    if (!Object.prototype.hasOwnProperty.call(this, '__call') && typeof this.__call === 'function') {
+      throw new TypeError('Class mast be implement method "__call"')
+    }
+
     return new Proxy(this, {
       apply: (target, thisArg, args) => target.__call(...args)
     })
